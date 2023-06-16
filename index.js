4444975c38aabd327d9d045ccc3ea743ac027f62
Jawad <jawad.azizi1398@gmail.com>
2023-06-16 09:44:05 +0430

    plugin added

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-// in the name of ullah
-console.log('cms starting')
-
 import {Router} from '@ulibs/router'
 import {PluginManager} from '@ulibs/plugin'
+
 let ctx = {}
 
+//add router
 const app = new Router()
+app.startServer('localhost', 5000, ()=>{
+    console.log('App is running on port 5000')
+})
 ctx['router'] = app
+
+
+
 
 const pm = PluginManager({config: './pm.config.json', ctx: ctx})
 
+//loads the docs and todos plugins that are inside plugins directory
 await pm.loadPlugins('./plugins')
+
+
 // pm.install('docs', import('./plugins/docs/index.js'))
 // pm.install('todos', import('./plugins/todos/index.js'))
-
 // await pm.enable('docs')
+
 await pm.start()
 
+// pm.remove('docs')

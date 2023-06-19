4514a31d924bd710b2a4d3b48e20655641c8118b
Jawad <jawad.azizi1398@gmail.com>
2023-06-20 00:01:00 +0430

    components docs added

---
+++ plugins/docs/routes/components/[subTitle]/index.js
@@ -0,0 +1,41 @@
+import { View, Card } from '@ulibs/components'
+import { tag } from '../../../../../../router/src/ui/tags.js'
+import pm from '../../../../../pluginManager.js'
+
+
+export const load= async(req)=>{
+    const subTitle = req.params.subTitle
+    const ctx = pm.getContext()
+    const Page = ctx.db.getModel('docs_examples')
+    const examples = await Page.query(
+        {
+            where: {
+                subTitle: {
+                    title: `${subTitle}:like`
+                }
+            },
+            select: {
+                title: true,
+                description: true,
+                code: true
+            },
+        }
+    )
+    req.examples = examples
+}
+
+export default function(req){
+    //returns the examples of the specific title
+    return View(
+    [
+        ...req.examples.data.map(example=>{
+            return View(
+                [
+                    tag('h1', {}, example.title),
+                    tag('p', {}, example.description),
+                    tag('code', {}, example.code),
+                ]
+            )
+        })
+    ])
+}
--- plugins/docs/routes/components/index.js
+++ plugins/docs/routes/components/index.js
@@ -1,8 +1,38 @@
 import { View, Card } from '@ulibs/components'
+import {tag} from '@ulibs/router'
+import pm from '../../../../pluginManager.js'
 
-export default function({}){
+
+export async function load(req){
+    const ctx = pm.getContext()
+    const Page = ctx.db.getModel('docs_subTitles')
+    const subTitles = await Page.query(
+        {
+            where: {
+                title: 'components:like'
+            },
+            select: {
+                title: true
+            }
+            
+        }
+    )
+    req.subTitles = subTitles
+}
+
+export default function(req){
     return View(
-    [
-        'this is components'
-    ])
+        [
+            //list of subtitles
+            tag('ul', {}, 
+            [
+                // req.subTitles is set in load function
+                ...req.subTitles.data.map(({title})=>{
+                    //each of the liks will load the examples at that title
+                    return tag('li', {}, [tag('a', {href: `/docs/components/${title}`}, title)])
+                })
+            ]
+            )
+        ]
+    )
 }
